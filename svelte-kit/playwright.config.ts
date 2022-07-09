bb0312d7326d16f8c2a1901fb29a32a36d192d0d
Risqi Romadhoni <risqiromadhoni@Risqis-MacBook-Pro.local>
2022-07-09 23:44:20 +0700

    svelte kit use yarn berry

---
+++ svelte-kit/playwright.config.ts
@@ -0,0 +1,10 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+	webServer: {
+		command: 'yarn run build && yarn run preview',
+		port: 3000
+	}
+};
+
+export default config;
